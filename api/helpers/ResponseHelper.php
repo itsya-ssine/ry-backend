8aa4f27063e8e60b8bf6d27e74f7c336898edbd7
Naoki <elmajdoubiyassine5@gmail.com>
2026-01-07 17:07:41 +0100

    allow front requests

--- api/helpers/ResponseHelper.php
+++ api/helpers/ResponseHelper.php
@@ -1,5 +1,15 @@
 <?php
 
+header("Access-Control-Allow-Origin: https://ry-frontend.vercel.app");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Credentials: true");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
 function sendResponse(array|bool|null $data, int $code = 200): void {
     if (ob_get_length()) ob_clean();
 
