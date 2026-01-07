d0ce74a21e4a9f7889a91ba1df736e28a896de0b
Naoki <elmajdoubiyassine5@gmail.com>
2026-01-07 17:16:07 +0100

    update

--- api/helpers/ResponseHelper.php
+++ api/helpers/ResponseHelper.php
@@ -1,19 +1,18 @@
 <?php
 
+header("Access-Control-Allow-Origin: https://ry-frontend.vercel.app");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Credentials: true");
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header("Access-Control-Allow-Origin: https://ry-frontend.vercel.app");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-    header("Access-Control-Allow-Credentials: true");
     http_response_code(204);
     exit;
 }
 
 function sendResponse($data, $code = 200) {
     header("Access-Control-Allow-Origin: https://ry-frontend.vercel.app");
-    header("Access-Control-Allow-Credentials: true");
     header('Content-Type: application/json; charset=UTF-8');
-    
     http_response_code($code);
     echo json_encode($data);
     exit;
