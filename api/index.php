d1068ac7aa759321f35cc89cd2a1547363a11c2a
Naoki <elmajdoubiyassine5@gmail.com>
2026-01-07 17:56:48 +0100

    update index.php

--- api/index.php
+++ api/index.php
@@ -6,16 +6,6 @@ require_once __DIR__ . "/helpers/ResponseHelper.php";
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
-if ($_SERVER['REQUEST_METHOD'] === "OPTIONS") {
-    http_response_code(204);
-    exit;
-}
-
 require_once "config/db.php";
 
 $controllers = [
