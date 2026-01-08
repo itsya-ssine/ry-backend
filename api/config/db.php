4d66730b915582113a18372e40b7e08ec86098c7
Naoki <elmajdoubiyassine5@gmail.com>
2026-01-08 23:46:14 +0100

    update db.php

--- api/config/db.php
+++ api/config/db.php
@@ -1,8 +1,14 @@
 <?php
-$host = "";
-$db   = "";
-$user = "";
-$pass = "";
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'] ?? null;
+$db   = $_ENV['DB_NAME'] ?? null;
+$user = $_ENV['DB_USER'] ?? null;
+$pass = $_ENV['DB_PASS'] ?? null;
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
@@ -10,9 +16,8 @@ try {
     $conn = new mysqli($host, $user, $pass, $db);
 } catch (mysqli_sql_exception $e) {
     http_response_code(500);
-    
     echo json_encode([
-        "error" => "Database connection failed",
+        "error" => "Database connection failed"
     ]);
     exit;
 }
